82086d718dfd0bffbe775d5cc2ea894db62621b6
gits2501 <gits2501@users.noreply.github.com>
2019-04-26 18:30:15 +0200

    Working on logic for getting all data from api endpoints for given crypto id and sending back that data to browser(user).

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 // node server
-require('dotenv').config();                // safely add coinmarket cap API key to process.env
+require('dotenv').config();                // safely add coinmarketcap API key to process.env
 
 const express = require('express');
-const rp = require('request-promise');
+const requestp = require('request-promise');
 const app = express();
 
 
@@ -15,11 +15,11 @@ const getCryptoData = function (){           // gets data from coinmarketcap
 
        process.nextTick(function cacheData(){
 
-            rp(coinmarketcapOptions).then(function(crypto_data){
+            requestp(listingOptions)
+            .then(function(crypto_data){
                    console.log("crypto data: \n", crypto_data)
                    crypto_cache = crypto_data;                  // put data we got to cache                    
-             }).catch(function(err){
-
+            }).catch(function(err){
                   console.log('API call error:', err.message);
             }); 
        })
@@ -29,9 +29,10 @@ const getCryptoData = function (){           // gets data from coinmarketcap
   cache_interval = 3600                   // one hour - not to waste free plan api points (will be set to 1min)
 }
 
-getCryptoData();
+//getCryptoData();
 
-const coinmarketcapOptions = {   //  set api request options 
+const listingOptions = {                //  set api request options for '/listings/latest' endpoint
+
   method: 'GET',
   uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
 
@@ -51,13 +52,47 @@ const coinmarketcapOptions = {   //  set api request options
 };
 
 
+   
+const allInfoOptions = {                //  set api request options for '/info' endpoint
+
+      method: 'GET',
+      uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/info',
+
+      qs: {
+        id: ''                                                     // id we will parse from client request
+      },
+
+      headers: {
+        'X-CMC_PRO_API_KEY': process.env.COINMARKETCAP_API_KEY
+      },
+
+      json: true,
+      gzip: true
+
+  // https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest
+}
+
 app.get('/listing', function getListing(req, res) {
-     if(crypto_cache)
+     if(typeof crypto_cache === "object")
          res.send(crypto_cache); 
      else
          res.send('Crypto cache data empty');
    
 })
-  
+
+app.get('/all', function getAllInfo(req, res){
+    let crypto_id = 1 // testing crypto id 1 , will be 'req.query.id';
+    
+    allInfoOptions.qs.id = crypto_id;   // set id we got from client request
+
+    requestp(allInfoOptions)
+    .then(function(crypto_data){
+        res.send(crypto_data);
+    })
+    .catch(function(err){
+        console.log('/all enpoint had an error: ', err)
+        res.send(err);
+    })
+});  
 
 app.listen(process.env.SERVER_PORT); console.log('Server UP')
